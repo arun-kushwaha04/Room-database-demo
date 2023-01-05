8d97ede578ee6933a271658f418593fc94032a9e
Arun Singh Kushwaha <arunkushwaha89765@gmail.com>
2023-01-06 01:58:25 +0530

    Added recycler view and its adapter

--- app/src/main/java/com/example/roomdbdemo/MainActivity.kt
+++ app/src/main/java/com/example/roomdbdemo/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.roomdbdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.roomdbdemo.db.WatchItem
 import com.example.roomdbdemo.db.WatchListDatabase
 
@@ -12,18 +16,60 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewModel : MainActivityViewModel
     private lateinit var toWatchName : EditText
     private lateinit var toWatchGenera : EditText
+    private lateinit var rvWatchList : RecyclerView
+    private lateinit var adapter: RecyclerViewAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         toWatchName = findViewById<EditText>(R.id.etName)
         toWatchGenera = findViewById<EditText>(R.id.etGenera)
+        rvWatchList = findViewById<RecyclerView>(R.id.rvWatchlist)
+        val addButton = findViewById<Button>(R.id.btnAdd)
+        val clearButton = findViewById<Button>(R.id.btnClear)
 
         val dao = WatchListDatabase.getInstance(this).WatchListDao()
         val factory = MainActivityViewModelFactory(dao);
         viewModel = ViewModelProvider(this,factory)[MainActivityViewModel::class.java]
 
+        initRecyclerView()
 
+        addButton.setOnClickListener{
+            if(validateInput()){
+                addToWatchList()
+                clearInput()
+            }else{
+                Toast.makeText(this,"All field are required", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        clearButton.setOnClickListener {
+            clearInput()
+        }
+    }
+
+    private fun initRecyclerView() {
+        rvWatchList.layoutManager = LinearLayoutManager(this)
+        adapter = RecyclerViewAdapter { selectedItem: WatchItem ->
+            deleteWatchItem(
+                selectedItem
+            )
+        }
+        rvWatchList.adapter = adapter
+
+            setRecyclerViewData()
+    }
+
+    private fun setRecyclerViewData() {
+        viewModel.watchList.observe(this) {
+            adapter.updateList(it!!)
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun validateInput() : Boolean{
+        return toWatchName.text.toString().isNotEmpty() && toWatchGenera.text.toString()
+            .isNotEmpty()
     }
 
     fun addToWatchList(){
---
+++ app/src/main/java/com/example/roomdbdemo/RecyclerViewAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.roomdbdemo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.roomdbdemo.db.WatchItem
+
+class RecyclerViewAdapter(private val clickHandler: (WatchItem)->Unit): RecyclerView.Adapter<MyViewHolder>() {
+
+    private val watchList:ArrayList<WatchItem> = ArrayList()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layoutInflater  = LayoutInflater.from(parent.context)
+        val layout = layoutInflater.inflate(R.layout.watch_item, parent, false)
+        return MyViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(watchList[position], clickHandler)
+    }
+
+    override fun getItemCount(): Int {
+        return watchList.size
+    }
+
+    fun updateList(newList : List<WatchItem>){
+        watchList.clear()
+        watchList.addAll(newList)
+    }
+}
+
+class MyViewHolder(private val view: View) : ViewHolder(view){
+    fun bind(watchItem: WatchItem, clickHandler: (WatchItem)->Unit){
+        val tvName = view.findViewById<TextView>(R.id.tvName)
+        val tvGenera = view.findViewById<TextView>(R.id.tvGenera)
+        val btnDelete = view.findViewById<Button>(R.id.btnDelete)
+
+        tvName.text = watchItem.name
+        tvGenera.text = watchItem.genera
+        btnDelete.setOnClickListener {
+            clickHandler(watchItem)
+        }
+    }
+}
